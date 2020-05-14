3387898089634e289da6e1c241c48c9d470ba60d
Zachary Boyet <support@dainbramaged.com>
2020-05-14 16:42:15 -0700

    t

--- src/StableCubeFileMuncherBundle.php
+++ src/StableCubeFileMuncherBundle.php
@@ -5,6 +5,7 @@ namespace StableCube\FileMuncherBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use StableCube\FileMuncherBundle\DependencyInjection\FilemuncherExtension;
 
+
 class StableCubeFileMuncherBundle extends Bundle
 {
     public function getContainerExtension()
